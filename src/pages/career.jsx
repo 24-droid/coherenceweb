bfc962d2931e381dad133da8c3811d236a933df0
Om Singh <omsingh06102005@gamil.com>
2025-01-11 20:53:23 +0530

    Made the header section of all pages immersive with the navbar except the contact,init page

--- src/pages/career.jsx
+++ src/pages/career.jsx
@@ -23,9 +23,9 @@ export default function CareerPage() {
       <Navbar />
 
      
-      <section className="w-full h-72 mt-20 bg-cover bg-center flex flex-row items-center justify-center text-white px-4 bg-highlight_background">
+      <section className="w-full h-72 mt-14 bg-cover bg-center flex flex-row items-center justify-center text-white px-4 bg-primary">
         <div className="text-center">
-        <h1 className="text-5xl font-bold text-black px-6 py-2 rounded-lg">
+        <h1 className="text-5xl font-bold text-white px-6 py-2 rounded-lg">
           Careers at Our Company
         </h1>
         <p className="text-lg font-medium mt-2 bg-black bg-opacity-50 px-4 py-2 rounded-lg">
